f5815e631ee713b75884f4cada23ea639add7465
Jakeeeee201 <151013251+Jakeeeee201@users.noreply.github.com>
2023-12-11 21:33:23 +0100

    Add files via upload

--- eindopdracht v2/scripts/script.js
+++ eindopdracht v2/scripts/script.js
@@ -1,5 +1,6 @@
 const headerBtn = document.querySelector('header button');
-
+const playBtn = document.querySelector('header > button');
+const circles = document.querySelectorAll('.circle');
 
 
 headerBtn.addEventListener('click', () => {
@@ -29,7 +30,42 @@ window.addEventListener('scroll', () => {
         header.classList.remove('header__sticky');
     }
 
-    console.log(scrollPos);
+    // console.log(scrollPos);
 
     prevScrollPos = scrollPos;
 });
+
+
+playBtn.addEventListener('click', () => {
+    circles.forEach(e => {
+        e.classList.toggle('is--active');
+    });
+});
+
+
+// hier staan alle js codes voor het veranderen van de achtergrond kleur per section
+// code geschreven door Hooft, M.S. t
+const sections = document.querySelectorAll('main section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(e => {
+        const intersecting = e.isIntersecting;
+
+        // if (intersecting == true) {
+        //     e.target.classList.add('is--active');
+        // } else if (intersecting == false) {
+        //     e.target.classList.remove('is--active');
+        // }
+
+        intersecting ? e.target.classList.add('is--active') : e.target.classList.remove('is--active');
+    });
+
+    // als de section .15 in beeld is dan wordt de code uitgevoerd
+}, {
+    threshold: .15
+})
+
+sections.forEach(section => {
+    observer.observe(section)
+})
+// hier eindigen alle js codes voor het veranderen van de achtergrond kleur per section
