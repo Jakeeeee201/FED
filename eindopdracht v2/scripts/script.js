03fe5a35c211e63eeab8c209923d2eabceffa6f6
Jakeeeee201 <151013251+Jakeeeee201@users.noreply.github.com>
2023-12-12 18:01:53 +0100

    Add files via upload

--- eindopdracht v2/scripts/script.js
+++ eindopdracht v2/scripts/script.js
@@ -1,25 +1,33 @@
-const headerBtn = document.querySelector('header button');
+
+// Header & hero variabelen
 const playBtn = document.querySelector('header > button');
 const circles = document.querySelectorAll('.circle');
+const playCube = document.querySelector('section.hero .cube__container');
+const playCubeOn = document.querySelector('section.hero .cube:nth-child(3)');
 
-
-headerBtn.addEventListener('click', () => {
-    headerBtn.classList.toggle('is--active');
-});
-
+// Navigatie variabelen
 let scrollPos = window.scrollY
 let prevScrollPos = window.scrollY
 let currentPos = 0;
 const header = document.querySelector('header');
 const headerHeight = header.offsetHeight
 
+// Service items variabelen
+const serviceItems = document.querySelectorAll('.service__list li');
+const body = document.body;
 
+
+
+/***********/
+/* Functie voor de navigatie toggle, wanneer je omhoog scrollt */
+/* Bron: ChatGPT als hulpmiddel gebruikt om een fout uit de code te halen */
+
+/***********/
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY || document.documentElement.scrollTop;
     const scrollUp = scrollPos < prevScrollPos;
 
     // scrollPos >= headerHeight ? header.classList.add('header__sticky') : header.classList.remove('header__sticky');
-
     // scrollUp ? header.classList.add('scrolled') : header.classList.remove('scrolled');
 
     if (scrollPos >= headerHeight) {
@@ -30,42 +38,70 @@ window.addEventListener('scroll', () => {
         header.classList.remove('header__sticky');
     }
 
-    // console.log(scrollPos);
-
     prevScrollPos = scrollPos;
 });
 
 
+
+/***********/
+/* Functie voor de play button state en toggle van play modus (button in de header)  */
+/***********/
 playBtn.addEventListener('click', () => {
+    playBtn.classList.toggle('is--active');
+    playBtn.firstElementChild.classList.toggle('rotate');
+    
+    playCube.firstElementChild.classList.toggle('rotate');
+    playCubeOn.classList.toggle('rotate');
+
     circles.forEach(e => {
         e.classList.toggle('is--active');
     });
 });
 
 
-// hier staan alle js codes voor het veranderen van de achtergrond kleur per section
-// code geschreven door Hooft, M.S. t
-const sections = document.querySelectorAll('main section');
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(e => {
-        const intersecting = e.isIntersecting;
+/***********/
+/* Functie voor de play button state en toggle van play modus  */
+/***********/
+playCube.addEventListener('click', () => {
+    playBtn.classList.toggle('is--active');
+    playBtn.firstElementChild.classList.toggle('rotate');
+    
+    playCube.firstElementChild.classList.toggle('rotate');
+    playCubeOn.classList.toggle('rotate');
 
-        // if (intersecting == true) {
-        //     e.target.classList.add('is--active');
-        // } else if (intersecting == false) {
-        //     e.target.classList.remove('is--active');
-        // }
 
-        intersecting ? e.target.classList.add('is--active') : e.target.classList.remove('is--active');
+    circles.forEach(e => {
+        e.classList.toggle('is--active');
     });
+});
 
-    // als de section .15 in beeld is dan wordt de code uitgevoerd
-}, {
-    threshold: .15
-})
 
-sections.forEach(section => {
-    observer.observe(section)
-})
-// hier eindigen alle js codes voor het veranderen van de achtergrond kleur per section
+
+/***********/
+/* Functie voor het openen van de service item elementen  */
+/* Bron: ChatGPT als hulpmiddel gebruikt voor het optimaliseren van de code  */
+/***********/
+serviceItems.forEach(e => {
+    e.addEventListener('click', (event) => {
+        // Om te voorkomen dat het bovenliggende element toegang krijgt tot de event
+        event.stopPropagation(); 
+
+        // Verwijdert de class van alle service items
+        serviceItems.forEach(item => {
+            if (item !== e) {
+                item.classList.remove('is--active');
+            }
+        });
+
+        // Voegt de class toe wanneer er op een service item geklikt wordt
+        e.classList.toggle('is--active');
+    });
+});
+
+body.addEventListener('click', () => {
+    // Verwijdert de class van alle service items wanneer er buiten de lijst gelikt wordt
+    serviceItems.forEach(e => {
+        e.classList.remove('is--active');
+    });
+});
